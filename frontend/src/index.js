0531ffb9578961c27a05d07a30c2062a4807f0e6
mohamed.yousef <engineer.mohamed.yossef@gmail.com>
2023-12-03 09:31:27 +0200

    working on layout

---
+++ frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
+import App from './Layout/App';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
