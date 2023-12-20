01b069830c1b24c57cfa952946a2d6004904adf0
mohamed.yousef <engineer.mohamed.yossef@gmail.com>
2023-12-20 11:11:20 +0200

    working on app

--- admin/shoppy_admin/dashboard/services.py
+++ admin/shoppy_admin/dashboard/services.py
@@ -32,7 +32,29 @@ class HomeService(generics.ModelService):
         return FetchAboutPageResponse(
             status=True,
             message="About page was loaded successfully !",
-            data="About page content"
+            data="""
+                <h3 className="mb-5 text-center">About Shoppy</h3>
+                
+                <article>
+                    Shoppy is a small e-commerce application , with basic functionality including ,
+                    register/login , categories , products search , cart operations , checkout and orders detail page.
+                    the application was built using React on the front side, Fiber for the backend APIs
+                    , Django for the dashboard.
+                </article>
+
+                <br />
+
+                <article>
+                    A SQLite3 database is used for storing the data , this could be replaced with any other SQL
+                    database of your choice like MySQL , PostgreSQL or MSSQL.
+                </article>
+
+                <br />
+
+                <article>
+                    For the installation instructions , check please the Github repo.
+                </article>
+            """
         )
 
     def SendContactUsMessage(self, request, context):
